c6af21582e8fcedbb0e5136eb10d21bc45ebf99c
Fb-developed <tupstadion8@gmail.com>
2025-05-06 16:11:13 +0300

    home-work-0.5-0.5

---
+++ home-work-0.5-0.5-2025.py
@@ -0,0 +1,266 @@
+# Input output 1.4
+
+
+# x = int(input())
+# y = (17 * ( x * x ) - 6 * x + 13)
+# print(f"{y}")
+
+# task-1
+
+
+# a = int(input())
+# b = int(input())
+# print(f"The quotient of the division is : {a // b}")
+# print(f"The remainder of the division is : {a % b}")
+
+# task 2
+
+# a = int(input())
+# b = int(input())
+# res = min(a, b)
+# print(f"{res}")
+
+# tsak-3
+
+
+# a = int(input())
+# b = int(input())
+# res = max(a, b)
+# print(f"{res}")
+
+# task-4
+
+
+# a = int(input())
+# print(f" In Month = {a * 12}")
+# print(f" In Day = {a * 365}")
+
+# task-5
+
+
+# a = int(input())
+# res = a % 2 + 3
+# print(f"{res}")
+
+# tsak-6
+
+
+# a = int(input())
+# b = int(input())
+# minn = min(a, b)
+# maxx = max(a, b)
+# print(f" {minn} {maxx}")
+
+# task-7
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# ac = abs(a - c)
+# bc = abs(b - c)
+# ab = abs(a - b)
+# abc = abs(ac + ab + bc)
+# print(f"AC={ac}")
+# print(f"BC={bc}")
+# print(f"AC+AB+BC={abc}")
+
+# task-8
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# volume = a * b * c
+# area = 2 * (a * b + b * c + a * c)
+# print(f"Volume = {volume}")
+# print(f"Surface Area = {area}")
+
+# task-9
+
+
+# l = int(input())
+# print(f"{l / 100}")
+
+# task-10
+
+
+# n = int(input())
+# n1 = n % 10
+# n2 = (n // 10) % 10
+# print(f"Last digit = {n1}")
+# print(f"Middle digit = {n2}")
+
+# task-11
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print(f"{(a + b + c) // 3}")
+
+# task-12
+
+
+# n = int(input())
+# print(f"{n} * {1} = {n * 1}")
+# print(f"{n} * {2} = {n * 2}")
+# print(f"{n} * {3} = {n * 3}")
+# print(f"{n} * {4} = {n * 4}")
+# print(f"{n} * {5} = {n * 5}")
+# print(f"{n} * {6} = {n * 6}")
+# print(f"{n} * {7} = {n * 7}")
+# print(f"{n} * {8} = {n * 8}")
+# print(f"{n} * {9} = {n * 9}")
+# print(f"{n} * {10} = {n * 10}")
+
+# task-13
+
+
+# n = int(input())
+# res = n // 3
+# print(f"Abdullo : {res // 2}")
+# print(f"Ahmadjon : {res * 2}")
+# print(f"Muhammad : {res // 2}")
+
+# task-14
+
+
+# n = int(input())
+# res = (n + 2) // 4
+# print(f"Basket1: {res * 2}")
+# print(f"Basket2: {res}")
+# print(f"Basket3: {res - 2}")
+
+# task-15
+
+
+
+# Input Output 1.5
+
+
+
+# n = int(input())
+# print(f"The next number for the number {n} is {n + 1}.")
+# print(f"The previous number for the number {n} is {n - 1}.")
+
+# task-1
+
+
+# a = int(input())
+# b = int(input())
+# res = abs(b // a)
+# print(f"{res}")
+
+# task-2
+
+
+# a = int(input())
+# b = int(input())
+# res = abs(b % a)
+# print(f"{res}")
+
+# task-3
+
+
+# n = int(input())
+# print(n % 10)
+
+# task-4
+
+
+# n = int(input())
+# print(n // 10)
+
+# task-5
+
+
+# n = int(input())
+# print(n // 10 % 10)
+
+# task-6
+
+
+# n = int(input())
+# n1 = n % 10
+# n2 = (n // 10) % 10
+# n3 = (n // 100) % 10
+# print(f"{n1 + n2 + n3}")
+
+# task-7
+
+
+# n = int(input())
+# print(f"{n // 60  % 24} {n % 60}")
+
+# task-8
+
+
+# n = int(input())
+# h = n // 3600 % 24
+# m = n // 60 % 60
+# s = n % 60
+# print(f"{h} : {m // 10}{m % 10} : {s // 10}{s % 10}")
+
+# task-9
+
+
+# rub = int(input())
+# kop = int(input())
+# n = int(input())
+# res = rub * 100 + kop
+# res1 = res * 3
+# print (f"{res1 // 100} {res1 % 100}")
+
+# task-10
+
+
+# c1 = int(input())
+# m1 = int(input())
+# s1 = int(input())
+# c2 = int(input())
+# m2 = int(input())
+# s2 = int(input())
+# time1 = c1 * 3600 + m1 * 60 + s1
+# time2 = c2 * 3600 + m2 * 60 + s2
+# res = abs(time1 - time2)
+# print(f"{res}")
+
+# task-11
+
+
+# a = int(input())
+# b = int(input())
+# res = b % a
+# print((a - res) % a)
+
+# task-12
+
+
+# n  = int(input())
+# m = int(input())
+# res = (m + n -1) // n
+# print (f"{res}")
+
+# task-13
+
+
+# a = int(input())
+# lt = a * 45
+# sb = (a // 2) * 5
+# lb = (a - 1) // 2 * 15
+# tt = lt + sb + lb
+# eh = 9 + tt // 60
+# em = tt % 60
+# print(f"{eh} {em}")
+
+# task-14
+
+
+# v = int(input())
+# t = int(input())
+# res = (v * t % 109 + 109) % 109
+
+# print(f"{res}")
+
+# task-15
