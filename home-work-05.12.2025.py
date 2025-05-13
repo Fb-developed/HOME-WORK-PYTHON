c9e4ece7878e3d837e9d14e3ba2504e67ad452cf
Fb-developed <tupstadion8@gmail.com>
2025-05-13 16:48:11 +0300

    home-work-0.5-12

---
+++ home-work-05.12.2025.py
@@ -0,0 +1,85 @@
+# numbers = [5, 2, 3, 4, 2]
+# if len(numbers) != len(set(numbers)):
+#     print("YES")
+# else:
+#     print("NO")
+
+# task-1
+
+
+
+
+
+
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {3, 5, 6, 7}
+# set3 = set1.intersection(set2)
+# print(tuple(set3))
+
+# task-2
+
+
+
+
+
+# set1 = {'apple', 'banana', 'apple', 'orange ', 'banana'}
+# set1 = sorted(set1)
+# print(set1)
+# print(tuple(set1))
+
+# task-3
+
+
+
+
+
+# list1 = [1, 2, 2, 3, 4, 4, 5]
+# list2 = []
+
+# for i in list1:
+#     if list1.count(i) >= 1 and i not in list2:
+#         list2.append(i)
+
+# print(list2)
+
+# task-4
+
+
+
+
+# set1 = {1, 2, 3, 4}
+# set2 = {3, 4, 5, 6}
+# set3 = set1.difference(set2)
+# print(tuple(set3))
+
+# task-5
+
+
+
+
+
+# list = [1, 2, 2, 3, 4, 4, 5, 5]
+# list1 = []
+# for i in list:
+#     if list.count(i) > 1:
+#         list1.append(i)
+# print(set(list1))
+
+# task-6
+
+
+
+
+
+# set1 = {1, 2, 2, 3, 4, 5, 6}
+# odd = []
+# even = []
+# for i in set1:
+#     if i % 2 == 0:
+#         even.append(i)
+#     else:
+#         odd.append(i)
+# print("Even numbers:", tuple(even))
+# print("Odd numbers:", tuple(odd))
+
+# task-7
