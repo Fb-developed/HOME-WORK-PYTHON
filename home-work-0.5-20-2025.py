db603e81782b90bcc88d3376eeed2a4aec78359c
Fb-developed <tupstadion8@gmail.com>
2025-05-21 15:50:55 +0300

    home-work-05-20

---
+++ home-work-0.5-20-2025.py
@@ -0,0 +1,46 @@
+# def func(a,b):
+#     if b == 0:
+#         return 0
+#     else:
+#         return a + func(a,b-1)
+# a = int(input())
+# b = int(input())
+# print(func(a,b))
+
+# task-1
+
+
+
+# def sum(n):
+#     if n < 10:
+#         return n
+#     else:
+#         return n % 10 + sum(n // 10)
+# n =int(input())
+# print(sum(n))
+
+# task-2
+
+
+
+
+# def count(n):
+#     if n < 10:
+#         return 1
+#     else:
+#         return 1 + count(n // 10)
+# n =int(input())
+# print(count(n))
+
+# task-3
+
+
+# def str(s):
+#     if len(s) == 1:
+#         return s
+#     else:
+#         return s[-1] + str(s[:-1])
+# s = input()
+# print(str(s))
+
+# task-4
