704b9e51b256e62749afb41d9a433cab985e53b4
Fb-developed <tupstadion8@gmail.com>
2025-05-28 15:05:13 +0300

    home-work-05-27

---
+++ home-work-05-27-2025.py
@@ -0,0 +1,44 @@
+from datetime import datetime as dt, date, time, timedelta
+
+
+# now_data= dt.now()
+# print("Date now:", now_data)
+
+# task-1
+
+
+
+# date1 = date(2025, 3, 25)
+# date2 = date(2025, 4, 10)
+# res = abs(date1 - date2).days
+# print(res)
+
+# task-2
+
+
+
+
+# date1 = date(2025, 3, 25)
+# res = date1.strftime("%A")
+# print(res)
+
+# task-3
+
+
+
+
+# date1 = date(2025, 3, 10)
+# n = int(input("Enter number for add days: "))
+# res = date1 + timedelta(days=n)
+# print(res)
+
+# task-4
+
+
+
+
+# date1 = date(2025, 3, 25)
+# res = date1.strftime("%d %B %Y")
+# print(res)
+
+# task-5
